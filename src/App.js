f1636cfac8528af7087699d5f4ffd15e089a7ab4
fsabiu <fsabiu@gmail.com>
2024-10-28 10:55:35 +0000

    Tab name and icon improved with Assistant name and Oracle logo.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ConfigPage from "./pages/ConfigPage";
 import ChatPage from "./pages/ChatPage";
 import Home from "./pages/Home";
+import APP_CONFIG from "./config/appConfig";
 
 function App() {
+  useEffect(() => {
+    // Set document title
+    document.title = APP_CONFIG.ASSISTANT_NAME;
+    
+    // Set favicon
+    const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+    link.type = 'image/png';
+    link.rel = 'icon';
+    link.href = APP_CONFIG.FAVICON_PATH;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }, []);
+
   return (
     <Router>
       <div className="App">
